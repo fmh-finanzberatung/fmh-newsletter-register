ef0b1f8e55377209ae8058da5f32300154b1fa12
mtkastner <kastner@galt.de>
2022-02-15 10:10:26 +0100

    added switch by hostname to allow turning off directory view in development

--- src/main-directory.js
+++ src/main-directory.js
@@ -2,14 +2,23 @@ import App from './AppDirectory.svelte';
 import EleAttr from 'mk-data-attribute';
 
 const folders = document.querySelectorAll('[data-dir]');
-
-
-
 const target = document.querySelector("#directory");
 const ele = EleAttr(target);
-const delay = ele.int('delay',0);
+const delay = ele.int('delay', 0);
+const devHost = ele.str('devHost', '');
+
+//don't use in development 
 
 setTimeout(() => {
+
+  console.log('devHost', devHost);
+  console.log('window.location.href', window.location.href);
+  console.log(window.location.href.indexOf(devHost));
+  if (window.location.href.indexOf(devHost) !== -1) {
+    console.log(`Component Viewer is not active because devHost ${devHost} is in the url`);
+    console.log(`Component Viewer not visible in development`);
+    return false;
+  }
 
   new App({
     target,
